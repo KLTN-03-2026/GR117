34467a2d7b69cbea49deea49f76fb7e00bd22d5a
Vincent <114419669+imhoanq@users.noreply.github.com>
2026-04-08 13:40:51 +0700

    Delete BE directory

--- BE/routes/admin.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyToken, authorizeRoles } = require('../middlewares/auth.middlewares.js');
-const adminController = require('../controllers/admin.controller.js');
-
-router.patch('/approve-provider/:id', verifyToken, authorizeRoles("admin"), adminController.approveProvider);
-
-module.exports = router;
